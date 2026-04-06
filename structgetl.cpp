5d5b0da289e44a86b92e9b683d8c7be0a854d92d
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-06 16:05:51 +0700

    menambahkan getline

--- structgetl.cpp
+++ structgetl.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 struct detailAlamat
@@ -17,30 +16,20 @@ struct Orang
 };
 
 int main() {
-    Orang mhs[3];
-
-    for (int i = 0; i <= 2; i++)
-    {
-        cout << "Nama : " ;
-        getline(cin, mhs[i].nama);
-        cout << "Alamat Desa: " ;
-        getline(cin, mhs[i].alamat.desa);
-        cout << "Alamat Kota: " ;
-        getline(cin, mhs[i].alamat.kota);
-        cout << "Umur : ";
-        cin >> mhs[i].umur;
-        cout << endl;
-        cin.ignore();
-    }
-
-    for (int i = 0; i <= 2; i++)
-    {
-    // tampilkan
-    cout << " Data Penduduk : " << endl;
-    cout << " Nama : " << mhs[i].nama << endl;
-    cout << " Alamat : " << mhs[i].alamat.desa << endl;
-    cout << " Alamat : " << mhs[i].alamat.kota << endl;
-    cout << " Umur : " << mhs[i].umur << endl;
+    Orang mhs;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+    cout << "Alamat Desa: " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat Kota: " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
     cout << endl;
-    }
+
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat.desa << endl;
+    cout << " Alamat : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
 }
