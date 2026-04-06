5537e3c1b5efcf61f8af02feb1c5e1435968e899
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-06 16:01:39 +0700

    menambahkan detailAlamat

---
+++ structNew2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+    cout << "Alamat Desa: " ;
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota: " ;
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat.desa << endl;
+    cout << " Alamat : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
